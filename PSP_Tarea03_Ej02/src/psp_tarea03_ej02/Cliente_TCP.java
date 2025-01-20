e571f51374f71e5bcb4041f07a13886ea1194800
David Cano <dcanoea@fpvirtualaragon.es>
2025-01-20 01:29:54 +0100

    fin ejercicio 2

---
+++ PSP_Tarea03_Ej02/src/psp_tarea03_ej02/Cliente_TCP.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package psp_tarea03_ej02;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author David Cano Escario
+ */
+public class Cliente_TCP {
+
+    /**
+     * @param args the command line arguments
+     */
+    static final String host = "localhost";
+    static final int puerto = 1500;
+
+    public static void main(String[] args) {
+        try {
+            //Se establece una conexión al servidor en el puerto 1500 de la máquina local (localhost)
+            Socket sCliente = new Socket(host, puerto);
+
+            //Nombre del fichero a buscar pasado por consola
+            Scanner s = new Scanner(System.in);
+            System.out.println("Introduce el nombre del fichero a buscar: ");
+            String ficheroBuscado = s.nextLine();
+
+            //Flujo de salida  para enviar el nombre del fichero al servidor
+            OutputStream output = sCliente.getOutputStream();
+            DataOutputStream flujoSalida = new DataOutputStream(output);
+            flujoSalida.writeUTF(ficheroBuscado); //se envia el nombre del fichero al servidor
+
+            //Flujo de entrada para leer los datos del servidor
+            InputStream input = sCliente.getInputStream();
+            DataInputStream flujoEntrada = new DataInputStream(input);
+
+            //Leer la respuesta del servidor
+            String respuesta = flujoEntrada.readUTF();
+            if (respuesta.equals("Encontrado")) {
+                System.out.println("Fichero encontrado. Contenido del fichero:");
+                String linea;
+                while (!(linea = flujoEntrada.readUTF()).equals("EOF")) {
+                    System.out.println(linea);
+                }
+            } else {
+                System.out.println(respuesta);  // Mensaje de error si el archivo no existe
+            }
+            
+            
+            //Cerramos todos los streams y sockets
+            flujoSalida.close();
+            output.close();
+            flujoEntrada.close();
+            input.close();
+            sCliente.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+}
---
+++ PSP_Tarea03_Ej02/src/psp_tarea03_ej02/Servidor_TCP.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package psp_tarea03_ej02;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.net.ssl.SSLServerSocket;
+
+/**
+ *
+ * @author David Cano Escario
+ */
+public class Servidor_TCP {
+
+    /**
+     * @param args the command line arguments
+     */
+    static final int puerto = 1500;
+
+    public static void main(String[] args) {
+        try {
+            // Creamos el objeto ServerSocket que escuchará en el puerto 1500
+            ServerSocket skServidor = new ServerSocket(puerto);
+            System.out.println("Servidor escuchando en puerto " + puerto);
+
+            //Esperamos una conexión entrante del cliente
+            Socket sCliente = skServidor.accept();
+            System.out.println("Cliente conectado");
+
+            //Flujo de entrada desde el cliente
+            InputStream input = sCliente.getInputStream();
+            DataInputStream flujoEntrada = new DataInputStream(input);
+
+            //Flujo de salida hacia el cliente
+            OutputStream output = sCliente.getOutputStream();
+            DataOutputStream flujoSalida = new DataOutputStream(output);
+
+            //Leer el nombre del fichero solicitado
+            String fichero = flujoEntrada.readUTF();
+            File file = new File(fichero);
+            if (file.exists()) {
+                flujoSalida.writeUTF("Encontrado");//Respuesta que se enviará al cliente
+                System.out.println("Fichero existe");
+
+                //Enviamos el contenido del fichero
+                FileReader fr = new FileReader(file);
+                BufferedReader br = new BufferedReader(fr);
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    flujoSalida.writeUTF(linea);//Se envía cada línea del archivo al cliente hasta que no haya más. Se usa el flujo de salida
+                }
+
+                flujoSalida.writeUTF("EOF");  // Marcador para indicar el fin del archivo
+                System.out.println("Fichero enviado al cliente");
+                br.close();
+                fr.close();
+            } else {
+                flujoSalida.writeUTF("El fichero no existe");//Respuesta que se enviará al cliente
+                System.out.println("Fichero no existe");
+            }
+            
+            sCliente.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
